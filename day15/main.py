433171a881714f89342c23533bc86c791143eafa
Christoph Oechslein <christoph.oechslein@garmin.com>
2020-12-15 09:49:10 +0100

    Day 15 cleanup
    Change-Id: I916ad1fb6f497231e6f37ee1c3c3155404d9956d

--- day15/main.py
+++ day15/main.py
@@ -1,4 +1,4 @@
-from typing import List, Mapping, Dict
+from typing import List, Dict
 
 from input import TEST_INPUT, PUZZLE_INPUT, TEST_INPUT_2ND, PUZZLE_INPUT_2ND
 
@@ -19,10 +19,8 @@ def get_number_spoken(nth_number_spoken: int, turns: List[int]):
         numbers_spoken[spoken_number] = curr_turn
         curr_turn += 1
     previous_spoken_number: int = turns[-1]
-    while True:
-        curr_turn += 1
-        if curr_turn > nth_number_spoken:
-            return previous_spoken_number
+    curr_turn += 1
+    while curr_turn <= nth_number_spoken:
         # check if previous_spoken_number
         # - was never spoken => speak it
         # - was spoken => calc difference curr_turn - spoken_turn and speak it
@@ -33,6 +31,8 @@ def get_number_spoken(nth_number_spoken: int, turns: List[int]):
             new_number = (curr_turn - 1) - numbers_spoken[previous_spoken_number]
             numbers_spoken[previous_spoken_number] = curr_turn - 1
             previous_spoken_number = new_number
+        curr_turn += 1
+    return previous_spoken_number
 
 
 assert get_number_spoken(2020, parse_input(TEST_INPUT)) == 436
