e4d8e7d88e43ce98896a5490fb20510be5e165d5
Jacky <oechslein@gmx.de>
2020-12-19 23:42:20 +0100

    Day19 - reddit solution (convert to regexp)

--- day19/main_2nd.py
+++ day19/main_2nd.py
@@ -1,21 +1,11 @@
 import re
+from functools import lru_cache
 
 from input import PUZZLE_INPUT, TEST_INPUT_2ND, TEST_INPUT
 
 
-class alt_list(list):
-    pass
-
-
 class sequence_list(list):
     pass
-
-
-"""
-bbabbbbaabaabba: True and True [8, 42, 9, 14, 27, 14, 18, 15, 1, 15, 14, 14, 11, 42, 9, 14, 27, 14, 18, 15, 1, 15, 1, 14, 31, 1, 13, 1, 12, 19, 14, 14, 1]
-ababaaaaaabaaab: True and True [8, 42, 10, 23, 25, 1, 14, 1, 14, 1, 11, 42, 10, 28, 16, 15, 1, 1, 1, 1, 1, 31, 14, 17, 1, 7, 1, 21, 1, 14]
-ababaaaaabbbaba: True and True [8, 42, 10, 23, 25, 1, 14, 1, 14, 1, 11, 42, 9, 1, 26, 1, 20, 1, 15, 1, 14, 31, 14, 17, 14, 2, 1, 24, 14, 1]
-"""
 
 
 def parse_input(my_input, replace=True):
@@ -23,28 +13,13 @@ def parse_input(my_input, replace=True):
     if replace:
         rule_lines = rule_lines.replace('8: 42', '8: 42 | 42 8').replace('11: 42 31', '11: 42 31 | 42 11 31')
 
-    def make_alt_list(my_list):
-        my_list = list(my_list)
-        if len(my_list) == 1:
-            return my_list[0]
-        else:
-            return alt_list(my_list)
-
-    def make_sequence_list(my_list):
-        my_list = list(my_list)
-        if len(my_list) == 1:
-            return my_list[0]
-        else:
-            return sequence_list(my_list)
-
     def parse_alternative(alternative):
-        return make_sequence_list(
-            int(elem) if re.fullmatch(r'\d+', elem) else elem[1] for elem in alternative.split(' '))
+        return [int(elem) if re.fullmatch(r'\d+', elem) else elem[1] for elem in alternative.split(' ')]
 
     def parse_rule_line(rule_line):
         result = re.fullmatch(r'(\d+): (.*)', rule_line)
-        return int(result.group(1)), make_alt_list(parse_alternative(alternative)
-                                                   for alternative in result.group(2).split(' | '))
+        return int(result.group(1)), [parse_alternative(alternative)
+                                      for alternative in result.group(2).split(' | ')]
 
     rules_dict = dict(parse_rule_line(rule_line) for rule_line in rule_lines.split('\n'))
 
@@ -62,40 +37,38 @@ class GrammarChecker(object):
                 if test_line_index < len(test_line)}
 
     def match_rule_alt(self, current_possibilities, test_line, curr_rule):
+        current_possibilities = self.restrict_possibilities(current_possibilities, test_line)
+        if not current_possibilities:
+            return current_possibilities
         new_current_possibilities = set()
         for elem in curr_rule:
-            new_current_possibilities.update(self.match_rule(current_possibilities, test_line, elem))
+            new_current_possibilities.update(self.match_rule_seq(current_possibilities, test_line, elem))
         return new_current_possibilities
 
     def match_rule_seq(self, current_possibilities, test_line, curr_rule):
         for elem in curr_rule:
-            current_possibilities = self.match_rule(current_possibilities, test_line, elem)
+            if isinstance(elem, int):
+                current_possibilities = self.match_rule_int(current_possibilities, test_line, elem)
+            else:
+                assert isinstance(elem, str)
+                current_possibilities = self.match_rule_str(current_possibilities, test_line, elem)
         return current_possibilities
 
-    @staticmethod
-    def match_rule_str(current_possibilities, test_line, curr_rule):
+    def match_rule_str(self, current_possibilities, test_line, curr_rule):
+        current_possibilities = self.restrict_possibilities(current_possibilities, test_line)
+        if not current_possibilities:
+            return current_possibilities
         return {(test_line_index + 1, applied_rules)
                 for test_line_index, applied_rules in current_possibilities
                 if curr_rule == test_line[test_line_index]}
 
-    def match_rule(self, current_possibilities, test_line, curr_rule):
-        current_possibilities = self.restrict_possibilities(current_possibilities, test_line)
-        if not current_possibilities:
-            return current_possibilities
-        if isinstance(curr_rule, sequence_list):
-            return self.match_rule_seq(current_possibilities, test_line, curr_rule)
-        elif isinstance(curr_rule, alt_list):
-            return self.match_rule_alt(current_possibilities, test_line, curr_rule)
-        elif isinstance(curr_rule, int):
-            current_possibilities = {(test_line_index, applied_rules + (curr_rule,))
-                                     for test_line_index, applied_rules in current_possibilities}
-            return self.match_rule(current_possibilities, test_line, self.rules_dict[curr_rule])
-        else:
-            assert isinstance(curr_rule, str)
-            return self.match_rule_str(current_possibilities, test_line, curr_rule)
+    def match_rule_int(self, current_possibilities, test_line, curr_rule):
+        current_possibilities = {(test_line_index, applied_rules + (curr_rule,))
+                                 for test_line_index, applied_rules in current_possibilities}
+        return self.match_rule_alt(current_possibilities, test_line, self.rules_dict[curr_rule])
 
     def match_rule_first(self, index, test_line, with_print=False):
-        possibilities = self.match_rule({(0, (0,))}, test_line, self.rules_dict[0])
+        possibilities = self.match_rule_alt({(0, (0,))}, test_line, self.rules_dict[0])
         solutions = {(test_line_index, applied_rules)
                      for test_line_index, applied_rules in possibilities
                      if test_line_index == len(test_line)}
@@ -124,6 +97,41 @@ def test_2nd():
     assert not grammar_checker.match_rule_first(0, "aaaabbaaaabbaaa")
     assert check(*parse_input(TEST_INPUT_2ND), with_print=True) == 12
 
+
+def reddit_solution(my_input):
+    inp_str = my_input.split('\n\n')
+    rls = {exp[:exp.find(':')]: exp[exp.find(':') + 2:]
+           for exp in inp_str[0].split('\n')}
+    exps = inp_str[1].split('\n')
+
+    # ugly but efficient (but no general solution)
+    @lru_cache(137)
+    def cmp(ind, dpth=0):
+        curr = rls[ind]
+        return ('' if dpth > 4
+                else (curr[1]
+                      if '"' in curr
+                      else (f'''({"|".join([''.join([(cmp(i)
+                                                      if i != ind
+                                                      else f'({cmp(ind, dpth + 1)})')
+                                                     for i in prt.split()])
+                                            for prt in curr.split('|')])})'''
+                            if '|' in curr
+                            else (r''.join([cmp(i) for i in curr.split()])))))
+
+    def first_part():
+        return sum(re.fullmatch(cmp('0'), exp) is not None for exp in exps)
+
+    def second_part():
+        rls['8'], rls['11'] = '42 | 42 8', '42 31 | 42 11 31'
+        return sum(re.fullmatch(cmp('0'), exp) is not None for exp in exps)
+
+    print(first_part())
+    cmp.cache_clear()
+    print(second_part())
+
+
+reddit_solution(PUZZLE_INPUT)
 
 test_1st()
 print(check(*parse_input(PUZZLE_INPUT, replace=False), with_print=True))
